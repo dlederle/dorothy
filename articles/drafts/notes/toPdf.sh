bf6c144eee95580546e8d06c0e199294f20ca81a
Dylan Lederle-Ensign <dylan.lederle.ensign@gmail.com>
2012-04-05 16:17:57 -0400

    dys4ia

---
+++ articles/drafts/notes/toPdf.sh
@@ -0,0 +1,2 @@
+markdown Dys4ia.txt > draft.html;
+htmldoc --headfootsize 8.0 --webpage --linkcolor black --linkstyle plain --format pdf14 --fontspacing 2 --fontsize 12 --toctitle "Dys4ia" --titlefile title.html --header t.1  --footer ... draft.html > dys4ia.pdf;rm draft.html
