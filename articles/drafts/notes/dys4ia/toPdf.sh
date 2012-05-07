ac6557b48b64f40ef2383ef7bafe4be96876d27a
Dylan Lederle-Ensign <dylan.lederle.ensign@gmail.com>
2012-05-07 16:33:31 -0400

    promweek old post

---
+++ articles/drafts/notes/dys4ia/toPdf.sh
@@ -0,0 +1,2 @@
+markdown dys4ia-final.txt > draft.html;
+htmldoc --headfootsize 8.0 --webpage --linkcolor black --linkstyle plain --format pdf14 --fontspacing 2 --fontsize 12 --toctitle "Dys4ia" --titlefile title.html --header t.1  --footer ... draft.html > dys4iaFinal.pdf;rm draft.html
